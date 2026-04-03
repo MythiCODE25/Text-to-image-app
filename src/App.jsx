bf11eb9f5ce21d17a19a003006d8070347e60594
Mahin <mahinpatel025@gmail.com>
2026-04-03 17:47:12 +0530

    fix: use absolute url for fetching hf inference api

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
     try {
       const response = await fetch(
-        "/api/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0",
+        "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0",
         {
           headers: {
             Authorization: `Bearer ${import.meta.env.VITE_HF_TOKEN}`,
